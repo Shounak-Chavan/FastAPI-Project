ff6c1f315298c9c274cb308127dccacf14a552cb
Shounak-Chavan <chavan.shounak04@gmail.com>
2026-01-06 11:57:52 +0530

    added app configurations

---
+++ app/core/config.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Settings:
+    PROJECT_NAME = 'Car_Api'
+    API_KEY = os.getenv('API_KEY','demo-key')
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    JWT_ALGORITHM = 'HS256'
+    REDIS_URL = os.getenv('REDIS_URL')
+    MODEL_PATH = 'app/models/model.pkl'
+
+
+settings = Settings()
